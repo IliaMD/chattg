2a6436aec491cf2d75798e3b28794b25268f8183
ILYA BABIKOV <ii-babikov@yandex.ru>
2022-10-21 01:45:03 +0300

    add basis structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,69 @@
 import React from "react";
 import styled from "styled-components";
 import { Card } from "./components";
+import { BsSearch } from "react-icons/bs";
 
 function App() {
   return (
     <Root>
-      <Card />
+      <Container>
+        <List>
+          <SearchBlock>
+            <Input placeholder="Search..." />
+            <BsSearchStyled />
+          </SearchBlock>
+
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </List>
+
+        <Chat>
+          <Header>
+            <AvatarImg />
+            <Name>Глеб</Name>
+          </Header>
+        </Chat>
+      </Container>
     </Root>
   );
 }
 
-/* const asds = styled.div`
-  width: ;
-`; */
-
 const Root = styled.div`
   width: 100%;
 `;
+
+const Container = styled.div`
+  display: flex;
+  margin: 15px 10px;
+`;
+
+const List = styled.div``;
+
+const SearchBlock = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  border-radius: 15px;
+  padding-left: 50px;
+`;
+
+const BsSearchStyled = styled(BsSearch)`
+  position: absolute;
+  left: 12px;
+  bottom: 4px;
+`;
+
+const Chat = styled.div``;
+
+const Header = styled.div``;
+
+const AvatarImg = styled.img.attrs({
+  src: `jhjh`,
+})``;
+
+const Name = styled.h2``;
 
 export default App;
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,21 @@
+import styled from "styled-components";
+
 export const Card = () => {
-  return <div>CARD</div>;
+  return (
+    <CardContainer>
+      <CardImg />
+      <CardTitle>Глеб Шарипов</CardTitle>
+      <CardLastMsg>В госдуму внесли законопроект</CardLastMsg>
+    </CardContainer>
+  );
 };
+
+const CardContainer = styled.div``;
+
+const CardImg = styled.img.attrs({
+  src: `jhjh`,
+})``;
+
+const CardTitle = styled.h2``;
+
+const CardLastMsg = styled.p``;
