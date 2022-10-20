c86fe37136188cb9becda589f7d179b1f5277249
ILYA BABIKOV <ii-babikov@yandex.ru>
2022-10-21 00:42:41 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { Card } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+      <Card />
+    </Root>
   );
 }
+
+/* const asds = styled.div`
+  width: ;
+`; */
+
+const Root = styled.div`
+  width: 100%;
+`;
 
 export default App;
